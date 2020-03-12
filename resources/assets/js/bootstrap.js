d6fa707030f24d20b78d3c33c985435382321a1b
Mithun Halder <mithun.tutorial.one@gmail.com>
2020-03-13 01:32:21 +0600

    laravel echo & pushr js added

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -69,3 +69,18 @@ window.flash = function (message, level = 'success') {
     window.events.$emit('flash', { message, level });
 };
 
+
+
+
+
+import Echo from "laravel-echo"
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'abcdefg',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    disableStats: true,
+});
