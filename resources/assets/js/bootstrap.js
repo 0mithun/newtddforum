5c46f8251a7278082cdd81086e479eb0edd5a070
Mithun Halder <mithun.tutorial.one@gmail.com>
2020-06-15 17:14:13 +0600

    Update wsPort & wssPort

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -77,8 +77,8 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
-    wsPort: process.env.PUSHER_WS_PORT | 6001,
-    wssPort: process.env.PUSHER_WSS_PORT | 6001,
+    wsPort: process.env.MIX_PUSHER_WS_PORT | 6001,
+    wssPort: process.env.MIX_PUSHER_WSS_PORT | 6001,
     disableStats: true,
     enabledTransports:['ws','wss'],
 });
