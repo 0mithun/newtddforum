ce42456569231f6909b1d404de61193ff182ee05
Mithun Halder <mithun.tutorial.one@gmail.com>
2020-09-25 22:42:47 +0600

    fixed threads index page pagination

--- resources/views/threads/_pagination.blade.php
+++ resources/views/threads/_pagination.blade.php
@@ -1,27 +1,33 @@
 @php
     $totalPage = ceil($total_records / $per_page);
+    $link_limit = 12; // maximum number of links (a little bit inaccurate, but will be ok for now)
 @endphp
 
-<nav aria-label="...">
+@if ($totalPage > 1)
     <ul class="pagination">
-        @if($current_page != 1)
-      <li >
-        <a href="?page={{ $current_page - 1 }}"><span aria-hidden="true">&laquo;</span></a>
-      </li>
-
-
-      @endif
-
-      @for($page = 1; $page<=$totalPage; $page++)
-        <li class="{{ $current_page == $page ? 'active':'' }}">
-                <a href="?page={{ $page }}">{{ $page }}</a>
+        <li class="{{ ($current_page== 1) ? ' disabled' : '' }}">
+            <a href="?page=1">First</a>
+         </li>
+        @for ($i = 1; $i <= $totalPage; $i++)
+            @php
+            $half_total_links = floor($link_limit / 2);
+            $from = $current_page - $half_total_links;
+            $to = $current_page + $half_total_links;
+            if ($current_page < $half_total_links) {
+               $to += $half_total_links - $current_page;
+            }
+            if ($totalPage - $current_page < $half_total_links) {
+                $from -= $half_total_links - ($totalPage- $current_page) - 1;
+            }
+            @endphp
+            @if ($from < $i && $i < $to)
+                <li class="{{ ($current_page == $i) ? ' active' : '' }}">
+                    <a href="?page={{ $i }}">{{ $i }}</a>
+                </li>
+            @endif
+        @endfor
+        <li class="{{ ($current_page == $totalPage) ? ' disabled' : '' }}">
+            <a href="?page={{ $totalPage }}">Last</a>
         </li>
-      @endfor
-
-      @if($current_page != $totalPage)
-      <li >
-        <a href="?page={{ $current_page + 1 }}"><span aria-hidden="true">&raquo;</span></a>
-      </li>
-      @endif
     </ul>
-  </nav>
+@endif
