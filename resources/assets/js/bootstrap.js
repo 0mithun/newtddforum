883bb275bf01890b97467989983b1d9860f59aac
Mithun Halder <mithun.tutorial.one@gmail.com>
2020-09-26 18:53:20 +0600

    update for nginx reverse proxy

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -63,24 +63,24 @@ import Echo from "laravel-echo";
 
 window.Pusher = require("pusher-js");
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     wsHost: window.location.hostname,
-//     wsPort: 80,
-//     wssPort: 443,
-//     disableStats: true,
-//     enabledTransports:['ws','wss'],
-// });
-
 window.Echo = new Echo({
   broadcaster: "pusher",
   key: process.env.MIX_PUSHER_APP_KEY,
-  cluster: process.env.MIX_PUSHER_APP_CLUSTER,
   wsHost: window.location.hostname,
-  wsPort: 6001,
-  wssPort: 6001,
+  wsPort: 80,
+  wssPort: 443,
   disableStats: true,
   enabledTransports: ["ws", "wss"],
+  cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 });
+
+// window.Echo = new Echo({
+//   broadcaster: "pusher",
+//   key: process.env.MIX_PUSHER_APP_KEY,
+//   cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//   wsHost: window.location.hostname,
+//   wsPort: 6001,
+//   wssPort: 6001,
+//   disableStats: true,
+//   enabledTransports: ["ws", "wss"],
+// });
